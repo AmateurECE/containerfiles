f2aa1693191c3e0ee7f386fc41d7486c4355b4f4
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-06-11 13:01:22 -0500

    Update compilations containerfile with std. cfg., static rules

--- compilations/compilations-entrypoint.sh
+++ compilations/compilations-entrypoint.sh
@@ -8,9 +8,10 @@
 #
 # CREATED:          06/04/2022
 #
-# LAST EDITED:      06/04/2022
+# LAST EDITED:      06/11/2022
 ###
 
+# Start the application server
 /bin/compilations -s "$SECRET_FILE" -c "$CONF_FILE"
 
 ###############################################################################
