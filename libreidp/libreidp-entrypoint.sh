e0d982f55a5ee02d414be926f4ad8a217f6f70e1
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-06-12 19:19:03 -0500

    Create basic Containerfile and Makefile for libreidp application

---
+++ libreidp/libreidp-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+###############################################################################
+# NAME:             libreidp-entrypoint.sh
+#
+# AUTHOR:           Ethan D. Twardy <ethan.twardy@gmail.com>
+#
+# DESCRIPTION:      Entrypoint script for the container.
+#
+# CREATED:          06/12/2022
+#
+# LAST EDITED:      06/12/2022
+###
+
+# Generate the configuration file (used by default, unless the file path is
+# overridden in the environment) based on the value of certain environment vars
+cat - >$DEFAULT_CONFIGURATION_FILE <<EOF
+ldap:
+  uri: $LDAP_URI
+  start_tls_enabled: $START_TLS_ENABLED
+http:
+  address: "0.0.0.0:$HTTP_PORT"
+EOF
+
+/bin/libreidp -c $CONFIGURATION_FILE
+
+###############################################################################
