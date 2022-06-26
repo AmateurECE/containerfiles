f7c8d0e900529b40d97845bcdb13ca9b6f863a38
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-06-26 13:19:13 -0500

    Update containerfile and supporting scripts to add Redis

--- libreidp/get-container-ip.sh
+++ libreidp/get-container-ip.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 ###############################################################################
-# NAME:             run-container.sh
+# NAME:             get-container-ip.sh
 #
 # AUTHOR:           Ethan D. Twardy <ethan.twardy@gmail.com>
 #
-# DESCRIPTION:      Start/stop containers necessary for this application.
+# DESCRIPTION:      Get the IP of the container.
 #
-# CREATED:          06/26/2022
+# CREATED:          06/13/2022
 #
 # LAST EDITED:      06/26/2022
 ###
 
 if [ "0" != $(id -u) ]; then
-    >&2 printf '%s\n' "Error! This script must be run as root"
+    >&2 printf '%s\n' "Error: This script must be run as root!"
     exit 1
 fi
 
-export DOCKER_HOST=unix:///var/run/podman/podman.sock
 CONTAINER_NAME=edtwardy-webservices_libreidp_1
+NETWORK_NAME=edtwardy-webservices_front_net
+IP_QUERY="{{(index .NetworkSettings.Networks \"$NETWORK_NAME\").IPAddress}}"
 
-(podman ps | grep $CONTAINER_NAME) && docker-compose down
-docker-compose up -d
+podman inspect -f "IPv4: $IP_QUERY" $CONTAINER_NAME
 
 ###############################################################################
