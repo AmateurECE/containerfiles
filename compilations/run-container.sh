3e583c2180ee2d2e29c3b8849105d7bef807e4d1
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-07-27 17:58:03 -0500

    Don't automatically remove debug compilations container
    I've been seeing lately that this container dies randomly. With
    --rm specified to podman, the container is removed when the main
    process exits, which destroys any logs I could use to perform a
    postmortem.

--- compilations/run-container.sh
+++ compilations/run-container.sh
@@ -8,7 +8,7 @@
 #
 # CREATED:          06/13/2022
 #
-# LAST EDITED:      06/26/2022
+# LAST EDITED:      07/27/2022
 ###
 
 if [ "0" != $(id -u) ]; then
@@ -19,10 +19,11 @@ fi
 CONTAINER_NAME=edtwardy-webservices_compilations_1
 
 # Bring down the container if it's running
-(podman ps | grep $CONTAINER_NAME) && podman stop $CONTAINER_NAME
+(podman ps | grep $CONTAINER_NAME) && podman stop $CONTAINER_NAME \
+    && podman rm $CONTAINER_NAME
 
 # Restart it
-podman run -d --rm --name $CONTAINER_NAME \
+podman run -d --name $CONTAINER_NAME \
      --network edtwardy-webservices_front_net \
      -v compilations-secret:/data:ro \
      -e TZ=America/Chicago \
